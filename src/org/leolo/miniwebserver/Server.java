e561ffc64472e19ab9a46f078c990598ea05cdf1
LO Kam Tao Leo <leolo@leolo.org>
2017-12-09 11:01:19 +0800

    Provide error page when the server is overloaded

--- src/org/leolo/miniwebserver/Server.java
+++ src/org/leolo/miniwebserver/Server.java
@@ -3,17 +3,26 @@ package org.leolo.miniwebserver;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Properties;
+
+import org.leolo.miniwebserver.ErrorPageRepository.ErrorPageType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Server {
 	public static void main(String [] args) throws InterruptedException, IOException{
+		
+		Logger logger = LoggerFactory.getLogger(Server.class);
 		ServerSocket ss = new ServerSocket(8080);
+		ServerBusyErrorThread.getInstance();
+		Properties prop = new Properties();
+		prop.load(new java.io.FileInputStream("web.properties"));
+		ErrorPageRepository.getInstance().setReplaceSet(prop);
+		ErrorPageRepository.getInstance().loadErrorPage(503,"503.html",ErrorPageType.STATIC);
+		logger.info("Server ready");
 		while(true){
 			Socket s = ss.accept();
-			if(ServerThread.getThreadCount() < 100){
-				new ServerThread(s).start();
-			}else{
-				s.close();
-			}
+			ServerBusyErrorThread.getInstance().addEntry(s);
 		}
 	}
 }
---
+++ src/org/leolo/miniwebserver/ServerBusyErrorThread.java
@@ -0,0 +1,104 @@
+package org.leolo.miniwebserver;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class ServerBusyErrorThread extends Thread {
+	
+	private static ServerBusyErrorThread  instance = null;
+	Logger logger = LoggerFactory.getLogger(ServerBusyErrorThread.class);
+	public static final int MAX_SIZE = 100;
+	private int queueSize = 0;
+	private Queue<Entry> queue;
+	
+	public synchronized static ServerBusyErrorThread getInstance(){
+		if(instance == null)
+			instance = new ServerBusyErrorThread();
+		return instance;
+	}
+	
+	private ServerBusyErrorThread(){
+		logger.info("ServerBusyErrorThread is created");
+		queue = new LinkedList<>();
+		this.start();
+	}
+	
+	class Entry{
+		public Entry(Socket socket) {
+			super();
+			this.time = System.currentTimeMillis();
+			this.socket = socket;
+		}
+		long time;
+		Socket socket;
+	}
+	
+	public void addEntry(Socket s){
+		logger.info("New Entry");
+		if(queueSize>MAX_SIZE){
+			try {
+				s.close();
+			} catch (IOException e) {
+				logger.error(e.getMessage(),e);
+			}
+		}
+		synchronized(this){
+			this.queue.add(new Entry(s));
+			queueSize++;
+			this.notify();
+		}
+		
+	}
+	
+	public void run(){
+		//Master Loop
+		while(true){
+			Socket s = null;
+			if(queueSize==0){
+				synchronized(this){
+					try {
+						logger.info("Waiting for new entry");
+						wait();
+					} catch (InterruptedException e) {
+						logger.error(e.getMessage(),e);
+					}
+					logger.info("Has entry");
+				}
+			}
+			synchronized(this){
+				Entry e = queue.poll();
+				if(e!=null){
+					s = e.socket;
+					logger.info("Wait time {} ms",System.currentTimeMillis() - e.time);
+				}else{
+					continue;
+				}
+			}
+//			try {
+//				//Debug code
+//				Thread.sleep(2000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				logger.error(e.getMessage(),e);
+//			}
+			try {
+				PrintWriter out = new PrintWriter(s.getOutputStream());
+				out.println("HTTP/1.1 503 Service Unavailable");
+				out.println("Content-type: text/html");
+				out.println("Connection: closed");
+				out.println();
+				out.println(ErrorPageRepository.getInstance().getErrorPage(503));
+				out.flush();
+				s.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
